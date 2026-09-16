05a37d205d6747716f8a8619aed1e4627a2202b3
jessemj117-lgtm <jessemj117@gmail.com>
2026-09-16 21:01:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 // GitHub Pages + Supabase
 // ==========================================
 
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_PROJECT_URL";
-const SUPABASE_KEY = "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY";
+const SUPABASE_URL = "https://ropiudyalwarmowaiugu.supabase.co";
+const SUPABASE_KEY = "sb_publishable_m4JSo5oRhn6GUOrWzBJBtA_o-W3Fr8K";
 
 const { createClient } = supabase;
 
